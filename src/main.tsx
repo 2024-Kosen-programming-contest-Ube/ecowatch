cd41eb69ff2c511d9c711b6baf11ad84753f4d3f
Kogepan229 <kogepan25629@gmail.com>
2024-08-11 17:47:30 +0900

    Implement routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MenuPage from "./pages/menu/page.tsx";
+import MainPage from "./pages/main/page.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<MenuPage />} />
+          <Route path="main" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
---
+++ src/pages/menu/page.tsx
@@ -0,0 +1,5 @@
+function MenuPage() {
+  return <></>;
+}
+
+export default MenuPage;
