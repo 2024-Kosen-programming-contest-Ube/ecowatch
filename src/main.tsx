9a2d79dfc5c78b5641d6becbb5cb63ba15ba75e7
Kogepan229 <kogepan25629@gmail.com>
2024-08-11 22:50:14 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MenuPage from "./pages/menu/page.tsx";
-import MainPage from "./pages/main/page.tsx";
+import "./styles/global.css";
+import MenuPage from "./pages/menu/menu.tsx";
+import MainPage from "./pages/main/main.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet" />
+    </head>
     <BrowserRouter>
       <Routes>
         <Route path="/">
