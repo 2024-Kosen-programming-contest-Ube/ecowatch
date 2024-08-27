9cd34823cd797b9a1657f4726b69851670621304
Kogepan229 <kogepan25629@gmail.com>
2024-08-27 22:06:17 +0900

    Remove body margin

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,3 +1,4 @@
 import { globalStyle } from "@vanilla-extract/css";
 
 globalStyle("html *", { fontFamily: "Noto Sans JP, sans-serif", fontStyle: "normal", fontOpticalSizing: "auto" });
+globalStyle("body", { margin: "0px" });
