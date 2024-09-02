2878787bc970aeba086f5e70eda6286ae0d068a9
Kogepan229 <kogepan25629@gmail.com>
2024-09-02 23:43:00 +0900

    Use react-helmet-async

--- packages/classroom/src/main.tsx
+++ packages/classroom/src/main.tsx
@@ -7,22 +7,25 @@ import MainPage from "./pages/main/main.tsx";
 import AttendPage from "./pages/attend/attend.tsx";
 import LoginPage from "./pages/login/login.tsx";
 import { HeadFont, HeadIcon } from "@ecowatch/ui";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <head>
-      <HeadFont />
-      <HeadIcon />
-    </head>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route index element={<MenuPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="main" element={<MainPage />} />
-          <Route path="attend" element={<AttendPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet>
+        {HeadFont()}
+        {HeadIcon()}
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<MenuPage />} />
+            <Route path="login" element={<LoginPage />} />
+            <Route path="main" element={<MainPage />} />
+            <Route path="attend" element={<AttendPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 );
--- packages/student/src/main.tsx
+++ packages/student/src/main.tsx
@@ -3,17 +3,20 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "@ecowatch/ui/src/global.css.ts";
 import { HeadFont, HeadIcon } from "@ecowatch/ui";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <head>
-      <HeadFont />
-      <HeadIcon />
-    </head>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/"></Route>
-      </Routes>
-    </BrowserRouter>
+    <HelmetProvider>
+      <Helmet>
+        {HeadFont()}
+        {HeadIcon()}
+      </Helmet>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/"></Route>
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   </StrictMode>,
 );
